2bf526183a9a17838274a768e8181b917da94b61
sejarasakura <61192933+sejarasakura@users.noreply.github.com>
2020-11-10 22:13:48 +0800

    update

--- WebApplication/src/java/entityClass/Plate.java
+++ WebApplication/src/java/entityClass/Plate.java
@@ -9,6 +9,36 @@ package entityClass;
  *
  * @author ITSUKA KOTORI
  */
-public class Plate {
+public class Plate extends AbstractEntity{
+
+    @Override
+    public boolean isNotNull() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean split(String rowData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int compareTo(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }

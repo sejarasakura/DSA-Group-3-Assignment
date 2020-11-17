b8fe65a6ea8de05d274de12c5efce82ab2894e47
sejarasakura <61192933+sejarasakura@users.noreply.github.com>
2020-11-18 05:17:13 +0800

    update master style

--- WebApplication/web/theme/lib/Master.js
+++ WebApplication/web/theme/lib/Master.js
@@ -2,8 +2,15 @@
 $(document).ready(function () {
     $(".art-list").click(function () {
         $('.art-list').each(function (i, obj) {
-            $(this).removeClass("active")
+            $(this).removeClass("active");
         });
-        $(this).addClass("active")
-    })
+        $(this).addClass("active");
+    });
+});
+
+$.validator.addMethod("pwcheck", function(value) {
+   return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
+       && /[a-z]/.test(value) // has a lowercase letter
+       && /[A-Z]/.test(value) // has a lowercase letter
+       && /\d/.test(value); // has a digit
 });

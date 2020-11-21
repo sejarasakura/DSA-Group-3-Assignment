cc7db37684e8063220045d3df4f1acb20d12f6ca
hongwah <73833818+hongwah@users.noreply.github.com>
2020-11-22 05:26:26 +0800

    Update Datas.java

--- WebApplication/src/java/main/Datas.java
+++ WebApplication/src/java/main/Datas.java
@@ -5,56 +5,62 @@
  */
 package main;
 
-import entity.Driver;
-import entity.Booking;
-import entity.Car;
-import entity.Customer;
+import adt.ArrList;
 import adt.XHashedDictionary;
 import adt.XQueue;
-import adt.ArrList;
+import entity.Booking;
+import entity.Customer;
+import entity.Driver;
 
 /**
  *
  * @author ITSUKA KOTORI
  */
 public class Datas {
-    
 
     /**
      *
-     * @author ITSUKA KOTORI
-     * web page setting in the dictionary
+     * @author ITSUKA KOTORI web page setting in the dictionary
      */
     public static XHashedDictionary<String, Object> settings;
-    
+
+    // That's right.
+    public static final XHashedDictionary<String, Class> TYPE_SWITCH = new XHashedDictionary<String, Class>();
+
+    static {
+        TYPE_SWITCH.add("int", Integer.TYPE);
+        TYPE_SWITCH.add("long", Long.TYPE);
+        TYPE_SWITCH.add("double", Double.TYPE);
+        TYPE_SWITCH.add("float", Float.TYPE);
+        TYPE_SWITCH.add("bool", Boolean.TYPE);
+        TYPE_SWITCH.add("char", Character.TYPE);
+        TYPE_SWITCH.add("byte", Byte.TYPE);
+        TYPE_SWITCH.add("void", Void.TYPE);
+        TYPE_SWITCH.add("short", Short.TYPE);
+    }
+
     /**
      *
-     * @author ITSUKA KOTORI 
-     * all the online driver
+     * @author ITSUKA KOTORI all the online driver
      */
     public static ArrList<Driver> drivers;
-    
+
     /**
      *
-     * @author ITSUKA KOTORI 
-     * all the online customer
+     * @author ITSUKA KOTORI all the online customer
      */
     public static ArrList<Customer> customers;
-    
+
     /**
      *
-     * @author ITSUKA KOTORI 
-     * all the current request booking
+     * @author ITSUKA KOTORI all the current request booking
      */
     public static XQueue<Booking> currentBooking;
-    
+
     /**
      *
-     * @author ITSUKA KOTORI 
-     * doing start up
+     * @author ITSUKA KOTORI doing start up
      */
     public static int stsrtUp = main.Functions.startUpInitialData();
-    
-    
-    
+
 }
